0d30a974ed2cd4f49fe72219c9d60d1a0af7fa12
Griko Nibras <git@griko.id>
2020-10-04 23:58:58 +0700

    Update example routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 /** @type {[string,string,boolean][]} */
 module.exports = [
   ["Usage", "/usage"],
-  ["Social Image", "/social-image", true],
-  ["Social Image API", "/api/social-image", true],
+  [
+    "Social Image",
+    "/social-image?title=Hello, world!&description=This is an example description. The possibilities are endless!&path=/some/random/path",
+    true,
+  ],
+  [
+    "Social Image API",
+    "/api/social-image?title=Hello, world!&description=This is an example description. The possibilities are endless!&path=/some/random/path",
+    true,
+  ],
   ["Empty Page", "/empty"],
 ];
