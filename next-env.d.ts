809158beb9b7151f8a37ffc96579bbe1f8bb5536
Griko Nibras <git@griko.id>
2021-01-17 19:55:31 +0700

    feat: update env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,9 +1,16 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 
+// https://fettblog.eu/typescript-react/styles/#load-css-with-webpack
+declare module "*.css" {
+  interface IClassNames {
+    [className: string]: string;
+  }
+  const classNames: IClassNames;
+  export = classNames;
+}
+
 declare module "*.svg" {
-  const content: any;
+  const content: any = null;
   export default content;
 }
