6ca3fa9dbf4237e678aada4f1ce1b6cfb6e19c2f
seonglae <sungle3737@gmail.com>
2021-06-08 12:05:27 +0900

    remove unnecessary

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,7 +3,7 @@ import type { NextApiRequest } from 'next'
 export const IS_DEV = process.env.NODE_ENV === 'development'
 export const IS_NOT_PROD = process.env.NODE_ENV !== 'production'
 
-export function absoluteUrl(req?: NextApiRequest, localhostAddress = 'localhost:3000') {
+export function absoluteUrl(req?: NextApiRequest, localhostAddress = 'localhost:8000') {
   let host = (req?.headers ? req.headers.host : window.location.host) || localhostAddress
   let protocol = /^localhost(:\d+)?$/.test(host) ? 'http:' : 'https:'
 
