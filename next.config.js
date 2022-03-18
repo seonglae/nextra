fc4ec77dbb405637a175565b591644db8aeb94c0
Restyled.io <commits@restyled.io>
2022-03-18 15:54:09 +0000

    Restyled by clang-format

--- next.config.js
+++ next.config.js
@@ -3,39 +3,40 @@ module.exports = {
   async headers() {
     return [
       {
-        source: '/api/:path*',
-        headers: [
+        source : '/api/:path*',
+        headers : [
           {
-            key: 'Access-Control-Allow-Credentials',
-            value: 'true',
+            key : 'Access-Control-Allow-Credentials',
+            value : 'true',
           },
           {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
+            key : 'Access-Control-Allow-Origin',
+            value : '*',
           },
           {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+            key : 'Access-Control-Allow-Methods',
+            value : 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
           },
           {
-            key: 'Access-Control-Allow-Headers',
-            value: `X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version`,
+            key : 'Access-Control-Allow-Headers',
+            value :
+                `X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version`,
           },
         ],
       },
     ]
   },
-  webpack5: true,
+  webpack5 : true,
 
   // https://nextjs.org/docs/api-reference/next.config.js/react-strict-mode
-  reactStrictMode: true,
+  reactStrictMode : true,
 
   // https://nextjs.org/docs/api-reference/next.config.js/rewrites
   async rewrites() {
     return [
       {
-        source: '/social.png',
-        destination: '/api/social-image',
+        source : '/social.png',
+        destination : '/api/social-image',
       },
     ]
   },
