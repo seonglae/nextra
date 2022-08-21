47855bc3e45b178a12f3fb02a4f57fbb305ff0c5
Restyled.io <commits@restyled.io>
2022-08-21 14:53:54 +0000

    refactor: restyled by clang-format

--- site-config.js
+++ site-config.js
@@ -1,22 +1,22 @@
 const descriptionMd = `Next Chakra UI Typescript Template`
 
-const description = descriptionMd
-  .replace(/\[([^\]]+)\](\([^)]+\)|\[[^\]]+\])/g, '$1')
-  .replace(/\n/g, '')
-  .replace(/\s{2,}/g, ' ')
-  .trim()
+const description =
+    descriptionMd.replace(/\[([^\]]+)\](\([^)]+\)|\[[^\]]+\])/g, '$1')
+        .replace(/\n/g, '')
+        .replace(/\s{2,}/g, ' ')
+        .trim()
 
 module.exports = {
-  title: 'Nextra',
+  title : 'Nextra',
   descriptionMd,
   description,
-  url: 'https://nextra-ts.vercel.app',
-  twitterUsername: '@SeonglaeC',
-  email: 'seongle3737@gmail.com',
-  socials: {
-    GitHub: 'https://github.com/seonglae/nextra',
-    Twitter: 'https://twitter.com/SeonglaeC',
+  url : 'https://nextra-ts.vercel.app',
+  twitterUsername : '@SeonglaeC',
+  email : 'seongle3737@gmail.com',
+  socials : {
+    GitHub : 'https://github.com/seonglae/nextra',
+    Twitter : 'https://twitter.com/SeonglaeC',
   },
-  bgColor: '#1A202C',
-  themeColor: '#46c0aE',
+  bgColor : '#1A202C',
+  themeColor : '#46c0aE',
 }
