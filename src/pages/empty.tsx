64f54ca534174a9e01ec625eefd365cce985d549
seonglae <sungle3737@gmail.com>
2023-04-24 17:46:15 +0900

    fix: next removed page type applyed correctly

--- src/pages/empty.tsx
+++ src/pages/empty.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { NextSeo } from 'next-seo'
 
-import type { NextPage } from '@/types/next'
+import type { NextPage } from 'next'
 
 const EmptyPage: NextPage = () => {
   return <NextSeo title="Empty" />
